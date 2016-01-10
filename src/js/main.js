37bae63219ce76d366713e28af6245aff6834590
Jun <felipe.jun.ohira@gmail.com>
2016-01-10 15:23:58 -0200

    Refactoring the main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,26 @@
 import $ from 'jquery';
 
+import MenuController from './controllers/menu_controller';
+
 import LeftMenu from './views/left_menu';
 import GameMap from './views/game_map';
 
 import CellMap from './models/cell_map';
-import Position from './value_objects/position';
 
 $(document).ready(function() {
+
   let leftMenu = new LeftMenu('#left-menu');
-
   let cellMap = new CellMap(80, 100);
-
   let gameMap = new GameMap({
     selector: '#game-map',
     rows: cellMap.rows,
     columns: cellMap.columns
   });
 
-  let cellListener = {
-    onCellClicked: cell => {
-      let position = cell.position;
-
-      let cellSelected = cellMap.getCellOnPosition(position);
-      cellSelected.toogleState();
-      if (cellSelected.isLive()) {
-        gameMap.setPositionAsLive(cellSelected.position);
-      } else {
-        gameMap.setPositionAsDead(cellSelected.position);
-      }
-    }
-  };
-
-  gameMap.cellListener = cellListener;
-
-  leftMenu.nextButton.click(updateGame);
-  leftMenu.runButton.click(() => {
-    setInterval(updateGame, 100);
+  let menuController = new MenuController({
+    leftMenu: leftMenu,
+    cellMap: cellMap,
+    gameMap: gameMap
   });
-
-  function updateGame() {
-    cellMap.updateAllCells();
-    cellMap.forEachCell(cell => {
-      if (cell.isLive()) {
-        gameMap.setPositionAsLive(cell.position);
-      } else {
-        gameMap.setPositionAsDead(cell.position);
-      }
-    });
-  }
 
 });
