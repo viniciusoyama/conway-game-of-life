9c7a0c3fa5bfed45ecd93091aaa2f3ad7d5f96c5
Vinicius Oyama <vinicius.oyama@codus.com.br>
2016-01-18 07:26:04 -0200

    enabling image upload

--- src/js/main.js
+++ src/js/main.js
@@ -7,10 +7,12 @@ import GameMap from './views/game_map';
 
 import CellMap from './models/cell_map';
 
+var Fabric = window.fabric;
+
 $(document).ready(function() {
 
   let leftMenu = new LeftMenu('#left-menu');
-  let cellMap = new CellMap(80, 100);
+  let cellMap = new CellMap(100, 100);
   let gameMap = new GameMap({
     selector: '#game-map',
     rows: cellMap.rows,
@@ -25,4 +27,44 @@ $(document).ready(function() {
 
   gameMap.cellListener = menuController;
 
+  var canvas = new Fabric.Canvas('c');
+  canvas.setDimensions({width: 100, height: 100})
+
+  document.getElementById('imgLoader').onchange = function handleImage(e) {
+    var reader = new FileReader();
+    reader.onload = function (event){
+      var imgObj = new Image();
+
+      imgObj.src = event.target.result;
+      imgObj.onload = function () {
+        var image = new Fabric.Image(imgObj);
+        image.set({
+          angle: 0,
+          padding: 0,
+          cornersize:0,
+          height: 100,
+          width: 100,
+        });
+        canvas.centerObject(image);
+        canvas.add(image);
+        canvas.renderAll();
+        var imageData = image.canvas.getContext().getImageData(0,0,canvas.width,canvas.height).data;
+
+        var oficialIndex=0;
+        for(var i=0; i< imageData.length; i+=4) {
+          var r = imageData[i];
+          var g = imageData[i+1];
+          var b = imageData[i+2];
+          var a = imageData[i+3];
+          var shouldPaint = (r > 150) || (g > 150) || (b > 150) || (a > 150);
+          if (shouldPaint) {
+            gameMap._cellList[oficialIndex].setAsLive();
+            cellMap._cells[oficialIndex].toogleState();
+          }
+          oficialIndex +=1;
+        }
+      }
+    }
+    reader.readAsDataURL(e.target.files[0]);
+  }
 });
